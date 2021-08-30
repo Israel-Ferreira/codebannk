322325b9cb4d75c7ef9cf5093b11628a80a98088
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-08-30 10:38:58 -0300

    Criando o primeiro projeto da ifc3

---
+++ bank/domain/credit_card.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+
+type CreditCard struct {
+	ID string
+	Name string
+	Number string
+	Balance float64
+	Limit float64
+	ExpirationMonth uint32
+	ExpirationYear uint32
+	Cvv string
+	CreatedAt time.Time
+}
+
+
+
+func NewCreditCard() *CreditCard {
+	c := &CreditCard{}
+	c.ID = uuid.NewV4().String()
+	c.CreatedAt = time.Now()
+
+	return c
+}
