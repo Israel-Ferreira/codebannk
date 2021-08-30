9b358817fe7ed42a716edf60a193404415b4939d
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-08-30 18:03:07 -0300

    Repository Works!!!

---
+++ bank/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/Israel-Ferreira/codebank/domain"
+	"github.com/Israel-Ferreira/codebank/infra/repository"
+	"github.com/Israel-Ferreira/codebank/usecase"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db := setupDb()
+
+	defer db.Close()
+
+	repo := repository.NewTxnRepository(db)
+
+	cc := domain.NewCreditCard()
+	cc.Cvv = 255
+	cc.Number = "5161 6129 1985 9781"
+	cc.ExpirationMonth = 06
+	cc.ExpirationYear = 2022
+
+	if err := repo.CreateCreditCard(*cc); err != nil {
+		fmt.Println(err.Error())
+		log.Fatalln("Error ao criar o cartão de credito")
+	}
+
+	fmt.Println("Gravou no Banco!!!")
+
+}
+
+func SetupTxnUseCase(db *sql.DB) usecase.UseCaseTxn {
+	txnRepositoryDB := repository.NewTxnRepository(db)
+	return usecase.NewUseCaseTxn(txnRepositoryDB)
+}
+
+func setupDb() *sql.DB {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"db",
+		"5432",
+		"postgres",
+		"root",
+		"codebank",
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatalln("Error on connecting database")
+	}
+
+	return db
+
+}
