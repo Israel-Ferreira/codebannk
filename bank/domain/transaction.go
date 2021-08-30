9e12b5b69b8265259317ae0ad5f728f2d20e6dd3
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-08-30 11:17:49 -0300

    Create Transaction Model

--- bank/domain/transaction.go
+++ bank/domain/transaction.go
@@ -1,2 +1,33 @@
 package domain
 
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type Transaction struct {
+	ID           string
+	Amount       float64
+	Status       string
+	CreditCardId string
+	Store        string
+	CreatedAt    time.Time
+}
+
+func (txn *Transaction) ProcessAndValidate(creditCard *CreditCard) {
+	if (creditCard.Balance + txn.Amount) > creditCard.Limit {
+		txn.Status = "REJECTED"
+	} else {
+		txn.Status = "APPROVED"
+		creditCard.Balance += txn.Amount
+	}
+}
+
+func NewTransaction() *Transaction {
+	txn := &Transaction{}
+	txn.ID = uuid.NewV4().String()
+	txn.CreatedAt = time.Now()
+
+	return txn
+}
